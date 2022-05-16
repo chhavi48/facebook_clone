966919c8831dd13d1392a347a3a06b8723074287
Chhavi srivastav <>
2022-05-16 18:35:49 +0530

    login

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@
 import './App.css';
 import Feed from './Components/Feed';
 import Header from './Components/Header';
+import Login from './Components/Login';
 // import MessageSender from './Components/MessageSender';
 import Sidebar from './Components/Sidebar';
 import Widgets from './Components/Widgets';
 
 function App() {
+  const user="chhavi";
+
   return (
     <div className="App">
-    facebook baba
-    <Header/>
-    <div className='app_body'>
-      <Sidebar/>
-      <Feed/>
-     <Widgets/>
+     {!user ? 
+     (<Login/>
+     ):(
+       <>
+                  <Header/>
+           <div className='app_body'>
+             <Sidebar/>
+             <Feed/>
+            <Widgets/>
+           </div>
+       </>
+
+     )}
+   
     </div>
-    </div>
+
   );
+
 }
 
 export default App;
